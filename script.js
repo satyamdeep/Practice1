054e5d4dc2bd475725c82c01430d8605cc2ae75a
satyamdeep <satyamdeep3@gmail.com>
2024-02-20 18:36:12 +0530

    Practice1

---
+++ script.js
@@ -0,0 +1,16 @@
+//Print 5 times your name
+
+// let name = "satyam Deep";
+// for(i=1; i<=5;i++){
+//     console.log(name);
+// }
+
+// Sum the first n numbers
+
+let sum=0;
+let n = prompt("Enter a number")
+console.log("Number Entered", n);
+for(i=1;i<=n;i++){
+    sum = sum+i;
+}
+console.log("The sum of numbers is",sum);
